3877a642cf87804ee78e4320f72540683ef9dc09
Daniel Perez <daniel.perezh@usantoto.edu.co>
2024-01-11 16:48:21 -0500

    Rendimiento de imagenes 11/01/2024

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ResumenComponent } from './resumen/resumen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',  component: AppComponent, pathMatch: 'full' },
+  { path: 'portafolio', component: PortafolioComponent },
+  { path: 'sobre_mi', component: BlogComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'resumen', component: ResumenComponent },
+  { path : '**', redirectTo: '/',pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
